8da95dd974cd93de92e23bd2a6ccf7bc3f6a1e55
Manuel Reale <71377122+manuelreale@users.noreply.github.com>
2020-11-11 13:04:23 +0100

    lol

--- public/sketch.js
+++ public/sketch.js
@@ -1,16 +1,24 @@
 let socket= io();
+let myColor ='white';
 
 socket.on('connect', newConnection);
 
 socket.on('mouseBroadcast', drawOtherMouse);
+
+socket.on("color", setColor);
+
+function setColor(assignedColor){
+  myColor=assignedColor;
+}
 
 function newConnection(){
   console.log('your id: '+socket.id);
 }
 
 function drawOtherMouse(data){
-  fill('yellow');
-  ellipse(data.x,data.y, 10)
+  fill(data.color);
+  noStroke()
+  ellipse(data.x,data.y, 20)
 }
 
 function preload(){
@@ -29,13 +37,16 @@ function draw() {
 }
 
 function mouseMoved(){
+  push()
   noStroke()
-  fill('white');
+  fill(myColor);
   ellipse(mouseX,mouseY, 20);
+  pop()
 
   let message= {
   x: mouseX,
   y: mouseY,
+  color: myColor,
 };
 
 socket.emit("mouse", message);
