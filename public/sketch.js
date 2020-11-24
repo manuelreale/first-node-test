4a37233353b091e6c66dfef679f8eb6cac2309c3
Manuel Reale <71377122+manuelreale@users.noreply.github.com>
2020-11-24 10:12:41 +0100

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -59,6 +59,7 @@ function setup() {
 
   let padding= 2;
   let posX=(windowWidth/2)-(width/2)
+  let posY=(windowHeight/2)-(height/2)
 
   // put setup code here
   background('white')
@@ -68,169 +69,169 @@ function setup() {
 
   button = createButton("");
   button.mousePressed(color1)
-  button.position(posX+50+(25+padding), height-75);
+  button.position(posX+50+(25+padding), posY-75);
   button.size(25,25);
   button.style("background-color", "#000000");
 
   button = createButton("");
   button.mousePressed(color2)
-  button.position(posX+50+(25+padding), height-50+padding);
+  button.position(posX+50+(25+padding), posY-50+padding);
   button.size(25,25);
   button.style("background-color", "#ffffff");
 
   button = createButton("");
   button.mousePressed(color3)
-  button.position(posX+50+(25+padding)*2, height-75);
+  button.position(posX+50+(25+padding)*2, posY-75);
   button.size(25,25);
   button.style("background-color", "#808080");
 
   button = createButton("");
   button.mousePressed(color4)
-  button.position(posX+50+(25+padding)*2, height-50+padding);
+  button.position(posX+50+(25+padding)*2, posY-50+padding);
   button.size(25,25);
   button.style("background-color", "#C1BEC2");
 
   button = createButton("");
   button.mousePressed(color5)
-  button.position(posX+50+(25+padding)*3, height-75);
+  button.position(posX+50+(25+padding)*3, posY-75);
   button.size(25,25);
   button.style("background-color", "#800202");
 
   button = createButton("");
   button.mousePressed(color6)
-  button.position(posX+50+(25+padding)*3, height-50+padding);
+  button.position(posX+50+(25+padding)*3, posY-50+padding);
   button.size(25,25);
   button.style("background-color", "#FB0204");
 
   button = createButton("");
   button.mousePressed(color7)
-  button.position(posX+50+(25+padding)*4, height-75);
+  button.position(posX+50+(25+padding)*4, posY-75);
   button.size(25,25);
   button.style("background-color", "#7D8004");
 
   button = createButton("");
   button.mousePressed(color8)
-  button.position(posX+50+(25+padding)*4, height-50+padding);
+  button.position(posX+50+(25+padding)*4, posY-50+padding);
   button.size(25,25);
   button.style("background-color", "#FEFE04");
 
   button = createButton("");
   button.mousePressed(color9)
-  button.position(posX+50+(25+padding)*5, height-75);
+  button.position(posX+50+(25+padding)*5, posY-75);
   button.size(25,25);
   button.style("background-color", "#018004");
 
   button = createButton("");
   button.mousePressed(color10)
-  button.position(posX+50+(25+padding)*5, height-50+padding);
+  button.position(posX+50+(25+padding)*5, posY-50+padding);
   button.size(25,25);
   button.style("background-color", "#02FE02");
 
   button = createButton("");
   button.mousePressed(color11)
-  button.position(posX+50+(25+padding)*6, height-75);
+  button.position(posX+50+(25+padding)*6, posY-75);
   button.size(25,25);
   button.style("background-color", "#048181");
 
   button = createButton("");
   button.mousePressed(color12)
-  button.position(posX+50+(25+padding)*6, height-50+padding);
+  button.position(posX+50+(25+padding)*6, posY-50+padding);
   button.size(25,25);
   button.style("background-color", "#04FCFC");
 
   button = createButton("");
   button.mousePressed(color13)
-  button.position(posX+50+(25+padding)*7, height-75);
+  button.position(posX+50+(25+padding)*7, posY-75);
   button.size(25,25);
   button.style("background-color", "#010180");
 
   button = createButton("");
   button.mousePressed(color14)
-  button.position(posX+50+(25+padding)*7, height-50+padding);
+  button.position(posX+50+(25+padding)*7, posY-50+padding);
   button.size(25,25);
   button.style("background-color", "#0201FB");
 
   button = createButton("");
   button.mousePressed(color15)
-  button.position(posX+50+(25+padding)*8, height-75);
+  button.position(posX+50+(25+padding)*8, posY-75);
   button.size(25,25);
   button.style("background-color", "#800180");
 
   button = createButton("");
   button.mousePressed(color16)
-  button.position(posX+50+(25+padding)*8, height-50+padding);
+  button.position(posX+50+(25+padding)*8, posY-50+padding);
   button.size(25,25);
   button.style("background-color", "#FF01FD");
 
   button = createButton("");
   button.mousePressed(color17)
-  button.position(posX+50+(25+padding)*9, height-75);
+  button.position(posX+50+(25+padding)*9, posY-75);
   button.size(25,25);
   button.style("background-color", "#7F7F3F");
 
   button = createButton("");
   button.mousePressed(color18)
-  button.position(posX+50+(25+padding)*9, height-50+padding);
+  button.position(posX+50+(25+padding)*9, posY-50+padding);
   button.size(25,25);
   button.style("background-color", "#FCFD83");
 
   button = createButton("");
   button.mousePressed(color19)
-  button.position(posX+50+(25+padding)*10, height-75);
+  button.position(posX+50+(25+padding)*10, posY-75);
   button.size(25,25);
   button.style("background-color", "#063E3F");
 
   button = createButton("");
   button.mousePressed(color20)
-  button.position(posX+50+(25+padding)*10, height-50+padding);
+  button.position(posX+50+(25+padding)*10, posY-50+padding);
   button.size(25,25);
   button.style("background-color", "#02FD81");
 
   button = createButton("");
   button.mousePressed(color21)
-  button.position(posX+50+(25+padding)*11, height-75);
+  button.position(posX+50+(25+padding)*11, posY-75);
   button.size(25,25);
   button.style("background-color", "#037EFC");
 
   button = createButton("");
   button.mousePressed(color22)
-  button.position(posX+50+(25+padding)*11, height-50+padding);
+  button.position(posX+50+(25+padding)*11, posY-50+padding);
   button.size(25,25);
   button.style("background-color", "#81FEFD");
 
   button = createButton("");
   button.mousePressed(color23)
-  button.position(posX+50+(25+padding)*12, height-75);
+  button.position(posX+50+(25+padding)*12, posY-75);
   button.size(25,25);
   button.style("background-color", "#034081");
 
   button = createButton("");
   button.mousePressed(color24)
-  button.position(posX+50+(25+padding)*12, height-50+padding);
+  button.position(posX+50+(25+padding)*12, posY-50+padding);
   button.size(25,25);
   button.style("background-color", "#7F81FA");
 
   button = createButton("");
   button.mousePressed(color25)
-  button.position(posX+50+(25+padding)*13, height-75);
+  button.position(posX+50+(25+padding)*13, posY-75);
   button.size(25,25);
   button.style("background-color", "#4001FD");
 
   button = createButton("");
   button.mousePressed(color26)
-  button.position(posX+50+(25+padding)*13, height-50+padding);
+  button.position(posX+50+(25+padding)*13, posY-50+padding);
   button.size(25,25);
   button.style("background-color", "#FB0181");
 
   button = createButton("");
   button.mousePressed(color27)
-  button.position(posX+50+(25+padding)*14, height-75);
+  button.position(posX+50+(25+padding)*14, posY-75);
   button.size(25,25);
   button.style("background-color", "#7F4005");
 
   button = createButton("");
   button.mousePressed(color28)
-  button.position(posX+50+(25+padding)*14, height-50+padding);
+  button.position(posX+50+(25+padding)*14, posY-50+padding);
   button.size(25,25);
   button.style("background-color", "#FD7F42");
 
