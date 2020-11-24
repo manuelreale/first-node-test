19a07c9a5f80d3dd2996c2f99b65891d35ce33c7
Manuel Reale <71377122+manuelreale@users.noreply.github.com>
2020-11-24 19:05:09 +0100

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -413,6 +413,6 @@ socket.emit("mouse", message);
 function windowResized() {
   createButtons()
   centerCanvas();
-  //resizeCanvas(1000, 800);
-  background("white")
+  resizeCanvas(1000, 800);
+  //background("white")
 }
